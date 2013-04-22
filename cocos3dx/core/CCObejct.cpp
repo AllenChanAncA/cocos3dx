3f6cc580bc2d34d75bf4428a975cf41d5e4a37e9
Neil Fang <neil3d@126.com>
2013-04-21 19:29:48 -0700

    CCObject file

---
+++ cocos3dx/core/CCObejct.cpp
@@ -0,0 +1,12 @@
+﻿#include "CCObejct.h"
+
+namespace cocos3dx
+{
+	CCObject::CCObject(void)
+	{
+	}
+
+	CCObject::~CCObject(void)
+	{
+	}
+}//namespace cocos3dx
---
+++ cocos3dx/core/CCObejct.h
@@ -0,0 +1,15 @@
+﻿
+#ifndef _COCOS3DX_OBJECT_H_
+#define _COCOS3DX_OBJECT_H_
+
+namespace cocos3dx
+{
+	class CCObject
+	{
+	public:
+		CCObject(void);
+		virtual ~CCObject(void);
+	};
+}//namespace cocos3dx
+
+#endif //_COCOS3DX_OBJECT_H_
